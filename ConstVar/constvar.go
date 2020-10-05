6503e25c6d1d07d0a73c1f0b77b6c38fae03b144
IngridCBarbosa <ingridcarolinelima@hotmail.com>
2020-10-05 15:49:33 -0300

    add some importants comments

--- ConstVar/constvar.go
+++ ConstVar/constvar.go
@@ -10,6 +10,8 @@ func main() {
 	var raio = 3.2 // tipo de raio foi inferido pelo compilaodr
 
 	// FORMA REDUZIDA DE CRIA UMA VERIÁVEL
+	// QUANDO DEFINIDO DESSA FORMA É UMA VARIÁVEL E NÃO UMA CONSTANTE
+	// 1- SEMPRE QUE DEFINIR UMA VARIÁVEL UTILIZA-LA
 	area := PI * math.Pow(raio, 2)
 
 	fmt.Print("o resultado da area é :", area)
@@ -26,10 +28,12 @@ func main() {
 
 	fmt.Println(a, b, c, d)
 
+	// e = true e f = false
 	var e, f bool = true, false
 
 	fmt.Print(e, f)
 
+	// g = 2 , h = false e i = "epa"
 	g, h, i := 2, false, "epa"
 
 	fmt.Print(g, h, i)
