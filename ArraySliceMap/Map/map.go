b182e7e17df507d317ac98238492d3f3f0f23a0f
IngridCBarbosa <ingridcarolinelima@hotmail.com>
2020-10-09 22:30:15 -0300

    add exemple using Map part 1

--- ArraySliceMap/Map/map.go
+++ ArraySliceMap/Map/map.go
@@ -1,7 +1,29 @@
 package main
 
-func main() {
-	// var aprovados map[int]string
+import "fmt"
 
-	// aprovados[123456789]
+func main() {
+
+	// MAPS DEVEM SER INICIALIZADOS
+
+	// var aprovados map[int]string => ASSIM SÓ CRIA
+
+	aprovados := make(map[int]string)
+
+	aprovados[1234567897] = "Maria"
+	aprovados[9765432125] = "Pedro"
+	aprovados[5679123458] = "Ana"
+
+	fmt.Println(aprovados)
+
+	for cpf, nome := range aprovados {
+		fmt.Printf("%s (CPF: %d)\n", nome, cpf)
+	}
+
+	// PARA ACESSAR O ELEMENTO DO MAP
+	fmt.Println(aprovados[5679123458])
+
+	// PARA DELELTAR UM ELEMENTO NO MAP
+
+	delete(aprovados, 9765432125)
 }
