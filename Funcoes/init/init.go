9549f1c83585531fe25d06e43252e087957241f1
IngridCBarbosa <ingridcarolinelima@hotmail.com>
2020-10-14 22:41:45 -0300

    using function init

---
+++ Funcoes/init/init.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// EXECUTADA AUTOMATICAMENTE PARA FAZER QUALQUER TIPO DE EXECUÇÃO
+func init() {
+	fmt.Println("Inicializando...")
+}
+
+func main() {
+	fmt.Println("Main...")
+}
